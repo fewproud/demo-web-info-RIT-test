070fefda2dedef775e92c2399ab52919e748a949
Administrator <Administrator@mybridge1.fyre.ibm.com>
2021-03-08 13:52:03 +1100

    initial

---
+++ demo web info RIT test/Logical/web-info.scm/config.scm
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1615171154475"
+    createdUser="Administrator" id="6ad5bd89:1780f9a736f:-7da3"
+    type="service_component_resource" updatedTimestamp="1615171154475"
+    updatedUser="Administrator" version="1.1">
+    <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
